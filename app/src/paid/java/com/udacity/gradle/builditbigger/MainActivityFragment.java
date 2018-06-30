1db00995cc8b4fb45cfb7350db41062ef75c077e
Zane Rastrick <zanerast@gmail.com>
2018-06-30 19:18:12 +0100

    Paid and Free Flavors implemented

---
+++ app/src/paid/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,64 @@
+package com.udacity.gradle.builditbigger;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.example.jokedisplaylibrary.JokeDisplayActivity;
+import com.example.jokesourcelibrary.FunnyJokesSource;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MainActivityFragment extends Fragment implements AsyncEndpoint.AfterJokeLoad {
+
+    private final String LOG_TAG = MainActivityFragment.class.getSimpleName();
+
+    public MainActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.i(LOG_TAG, "MSG! onCreateView()");
+
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        Button btnLaunchJokeLib = root.findViewById(R.id.btn_tell_joke);
+        btnLaunchJokeLib.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tellJoke();
+            }
+        });
+
+        return root;
+    }
+
+    public void tellJoke() {
+        Log.i(LOG_TAG, "MSG! tellJoke()");
+
+//        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+
+
+        new AsyncEndpoint(this).execute();
+
+    }
+
+    @Override
+    public void jokeLoaded(String joke) {
+        Log.i(LOG_TAG, "MSG! jokeLoaded()");
+
+        Intent intent = new Intent(getActivity(), JokeDisplayActivity.class);
+        intent.putExtra(JokeDisplayActivity.JOKE_KEY, joke);
+
+        startActivity(intent);
+    }
+}
--- jokesourcelibrary/src/main/java/com/example/jokesourcelibrary/FunnyJokesSource.java
+++ jokesourcelibrary/src/main/java/com/example/jokesourcelibrary/FunnyJokesSource.java
@@ -1,37 +1,39 @@
 package com.example.jokesourcelibrary;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class FunnyJokesSource {
 
-    public static ArrayList<String> jokesArrayList;
+
 
     public static String getJoke(){
-        jokesArrayList = new ArrayList<>();
 
-        String jokeOne = "A man walks into a bar. \n“Ouch.”";
+
+        String jokeOne = "A man walks into a bar. \n Ouch.";
         String jokeTwo = "Helium walks into a bar. " +
-                "\nHe orders a drink and wonders why his parents decided to give him such an unusual name, " +
+                "\n He orders a drink and wonders why his parents decided to give him such an unusual name, " +
                 "as he can never find it on personalized souvenirs. Plus, baristas never, ever get it right.";
         String jokeThree = "Knock knock." +
-                "\nWho’s there?" +
-                "\nTo." +
-                "\nTo who?" +
-                "No, “to whom.”";
+                "\n Who is there?" +
+                "\n To." +
+                "\n To who?" +
+                "\n No, to whom.";
         String jokeFour = "A horse walks into a bar. " +
-                "\nSeveral of the patrons quickly get up and leave, realizing the potential danger in the situation.";
+                "\n Several of the patrons quickly get up and leave, realizing the potential danger in the situation.";
         String jokeFive = "What did the farmer say after he lost his tractor? " +
-                "\n“Where’s my tractor?";
+                "\n Where is my tractor?";
 
-        Collections.addAll(jokesArrayList, jokeOne, jokeTwo, jokeThree, jokeFour, jokeFive);
+        List<String> jokesList = Arrays.asList(jokeOne, jokeTwo, jokeThree, jokeFour, jokeFive);
 
         Random rand = new Random();
-        int jokeNumber = rand.nextInt(jokesArrayList.size());
+        int jokeNumber = rand.nextInt(jokesList.size());
 
         String joke;
-        joke = jokesArrayList.get(jokeNumber);
+        joke = jokesList.get(jokeNumber);
 
         return joke;
     }
